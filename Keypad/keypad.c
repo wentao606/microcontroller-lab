eb09116c3393dd8477f31388c0ebc07774e56ac5
chenxuefeii <chenxuefei2333@outlook.com>
2025-02-03 16:15:03 -0500

    week 2

---
+++ Keypad/keypad.c
@@ -0,0 +1,367 @@
+/**
+ * Hunter Adams (vha3@cornell.edu)
+ * 
+ * Keypad Demo
+ * 
+ * KEYPAD CONNECTIONS
+ *  - GPIO 9   -->  330 ohms  --> Pin 1 (button row 1)
+ *  - GPIO 10  -->  330 ohms  --> Pin 2 (button row 2)
+ *  - GPIO 11  -->  330 ohms  --> Pin 3 (button row 3)
+ *  - GPIO 12  -->  330 ohms  --> Pin 4 (button row 4)
+ *  - GPIO 13  -->     Pin 5 (button col 1)
+ *  - GPIO 14  -->     Pin 6 (button col 2)
+ *  - GPIO 15  -->     Pin 7 (button col 3)
+ * 
+ * VGA CONNECTIONS
+ *  - GPIO 16 ---> VGA Hsync
+ *  - GPIO 17 ---> VGA Vsync
+ *  - GPIO 18 ---> 470 ohm resistor ---> VGA Green 
+ *  - GPIO 19 ---> 330 ohm resistor ---> VGA Green
+ *  - GPIO 20 ---> 330 ohm resistor ---> VGA Blue
+ *  - GPIO 21 ---> 330 ohm resistor ---> VGA Red
+ *  - RP2040 GND ---> VGA GND
+ * 
+ * SERIAL CONNECTIONS
+ *  - GPIO 0        -->     UART RX (white)
+ *  - GPIO 1        -->     UART TX (green)
+ *  - RP2040 GND    -->     UART GND
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/sync.h"
+#include "hardware/spi.h"
+
+// VGA graphics library
+#include "vga16_graphics.h"
+#include "pt_cornell_rp2040_v1_3.h"
+
+
+// Keypad pin configurations
+#define BASE_KEYPAD_PIN 9
+#define KEYROWS         4
+#define NUMKEYS         12
+
+#define LED             25
+
+unsigned int keycodes[12] = {   0x28, 0x11, 0x21, 0x41, 0x12,
+                                0x22, 0x42, 0x14, 0x24, 0x44,
+                                0x18, 0x48} ;
+unsigned int scancodes[4] = {   0x01, 0x02, 0x04, 0x08} ;
+unsigned int button = 0x70 ;
+
+
+char keytext[40];
+int prev_key = 0;
+
+
+#define STATE_NOT_PRESSED 0
+#define STATE_MAYBE_PRESSED 1
+#define STATE_PRESSED 2
+#define STATE_MAYBE_NOT_PRESSED 3
+
+// BEEP
+// Low-level alarm infrastructure we'll be using
+#define ALARM_NUM 0
+#define ALARM_IRQ TIMER_IRQ_0
+
+// Macros for fixed-point arithmetic (faster than floating point)
+typedef signed int fix15 ;
+#define multfix15(a,b) ((fix15)((((signed long long)(a))*((signed long long)(b)))>>15))
+#define float2fix15(a) ((fix15)((a)*32768.0)) 
+#define fix2float15(a) ((float)(a)/32768.0)
+#define absfix15(a) abs(a) 
+#define int2fix15(a) ((fix15)(a << 15))
+#define fix2int15(a) ((int)(a >> 15))
+#define char2fix15(a) (fix15)(((fix15)(a)) << 15)
+#define divfix(a,b) (fix15)( (((signed long long)(a)) << 15) / (b))
+
+//Direct Digital Synthesis (DDS) parameters
+#define two32 4294967296.0  // 2^32 (a constant)
+#define Fs 50000
+#define DELAY 20 // 1/Fs (in microseconds)
+
+// the DDS units - core 0
+// Phase accumulator and phase increment. Increment sets output frequency.
+volatile unsigned int phase_accum_main_0;
+volatile unsigned int phase_incr_main_0 = (400.0*two32)/Fs ;
+
+// DDS sine table (populated in main())
+#define sine_table_size 256
+fix15 sin_table[sine_table_size] ;
+
+// Values output to DAC
+int DAC_output_0 ;
+int DAC_output_1 ;
+
+// Amplitude modulation parameters and variables
+fix15 max_amplitude = int2fix15(1) ;    // maximum amplitude
+fix15 attack_inc ;                      // rate at which sound ramps up
+fix15 decay_inc ;                       // rate at which sound ramps down
+fix15 current_amplitude_0 = 0 ;         // current amplitude (modified in ISR)
+fix15 current_amplitude_1 = 0 ;         // current amplitude (modified in ISR)
+// Timing parameters for beeps (units of interrupts)
+#define ATTACK_TIME             250
+#define DECAY_TIME              250
+#define SUSTAIN_TIME            10000
+#define BEEP_DURATION           10500
+#define BEEP_REPEAT_INTERVAL    50000
+
+// State machine variables
+volatile unsigned int STATE_0 = 0 ;
+volatile unsigned int count_0 = 0 ;
+
+// SPI data
+uint16_t DAC_data_1 ; // output value
+uint16_t DAC_data_0 ; // output value
+
+// DAC parameters (see the DAC datasheet)
+// A-channel, 1x, active
+#define DAC_config_chan_A 0b0011000000000000
+// B-channel, 1x, active
+#define DAC_config_chan_B 0b1011000000000000
+
+//SPI configurations (note these represent GPIO number, NOT pin number)
+#define PIN_MISO 4
+#define PIN_CS   5
+#define PIN_SCK  6
+#define PIN_MOSI 7
+#define LDAC     8
+#define LED      25
+#define SPI_PORT spi0
+
+//GPIO for timing the ISR
+#define ISR_GPIO 2
+
+
+
+static void alarm_irq(void) {
+
+    // // Assert a GPIO when we enter the interrupt
+    // gpio_put(ISR_GPIO, 1) ;
+
+    // // Clear the alarm irq
+    // hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
+
+    // // Reset the alarm register
+    // timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY ;
+
+    if (STATE_0 == 0) {
+        // DDS phase and sine table lookup
+        phase_accum_main_0 += phase_incr_main_0  ;
+        DAC_output_0 = fix2int15(multfix15(current_amplitude_0,
+            sin_table[phase_accum_main_0>>24])) + 2048 ;
+
+        // Ramp up amplitude
+        if (count_0 < ATTACK_TIME) {
+            current_amplitude_0 = (current_amplitude_0 + attack_inc) ;
+        }
+        // Ramp down amplitude
+        else if (count_0 > BEEP_DURATION - DECAY_TIME) {
+            current_amplitude_0 = (current_amplitude_0 - decay_inc) ;
+        }
+
+        // Mask with DAC control bits
+        DAC_data_0 = (DAC_config_chan_B | (DAC_output_0 & 0xffff))  ;
+
+        // SPI write (no spinlock b/c of SPI buffer)
+        spi_write16_blocking(SPI_PORT, &DAC_data_0, 1) ;
+
+        // Increment the counter
+        count_0 += 1 ;
+
+        // State transition?
+        if (count_0 == BEEP_DURATION) {
+            STATE_0 = 1 ;
+            count_0 = 0 ;
+        }
+    }
+
+    // State transition?
+    else {
+        count_0 += 1 ;
+        if (count_0 == BEEP_REPEAT_INTERVAL) {
+            current_amplitude_0 = 0 ;
+            STATE_0 = 0 ;
+            count_0 = 0 ;
+        }
+    }
+
+    // De-assert the GPIO when we leave the interrupt
+    gpio_put(ISR_GPIO, 0) ;
+
+}
+static void scan(void){
+    
+}
+
+// This thread runs on core 0
+static PT_THREAD (protothread_core_0(struct pt *pt))
+{
+    // Indicate thread beginning
+    PT_BEGIN(pt) ;
+
+    // Some variables
+    static int i ;
+    static uint32_t keypad ;
+
+    // Add enum for debounce states
+    typedef enum {
+        RESET,
+        KEY_NOT_PRESSED,
+        KEY_MAYBE_PRESSED,
+        KEY_PRESSED,
+        KEY_MAYBE_NOT_PRESSED
+    } KeyState;
+
+    // Add these variables near other globals
+    static KeyState key_state = RESET;
+    
+
+    while(1) {
+
+        gpio_put(LED, !gpio_get(LED)) ;
+
+        // Scan the keypad!
+        for (i=0; i<KEYROWS; i++) {
+            // Set a row high
+            gpio_put_masked((0xF << BASE_KEYPAD_PIN),
+                            (scancodes[i] << BASE_KEYPAD_PIN)) ;
+            // Small delay required
+            sleep_us(1) ; 
+            // Read the keycode
+            keypad = ((gpio_get_all() >> BASE_KEYPAD_PIN) & 0x7F) ;
+            // Break if button(s) are pressed
+            if (keypad & button) break ;
+        }
+        // If we found a button . . .
+        if (keypad & button) {
+            // Look for a valid keycode.
+            for (i=0; i<NUMKEYS; i++) {
+                if (keypad == keycodes[i]) break ; // va
+            }
+            // If we don't find one, report invalid keycode
+            if (i==NUMKEYS) (i = -1) ;
+        }
+        // Otherwise, indicate invalid/non-pressed buttons
+        else (i=-1) ;
+
+        switch(key_state){
+            case RESET:
+
+        }
+
+
+
+        // Write key to VGA
+        if (i != prev_key) {
+            prev_key = i ;
+            fillRect(250, 20, 176, 30, RED); // red box
+            sprintf(keytext, "%d", i) ;
+            setCursor(250, 20) ;
+            setTextSize(2) ;
+            writeString(keytext) ;
+        }
+
+        // Print key to terminal
+        printf("\n%d", i) ;
+
+        PT_YIELD_usec(30000) ;
+    }
+    // Indicate thread end
+    PT_END(pt) ;
+}
+
+
+int main() {
+
+    // Initialize stdio
+    stdio_init_all();
+    printf("Hello, friends!\n");
+
+    // Initialize the VGA screen
+    initVGA() ;
+
+    // Draw some filled rectangles
+    fillRect(64, 0, 176, 50, BLUE); // blue box
+    fillRect(250, 0, 176, 50, RED); // red box
+    fillRect(435, 0, 176, 50, GREEN); // green box
+
+    // Write some text
+    setTextColor(WHITE) ;
+    setCursor(65, 0) ;
+    setTextSize(1) ;
+    writeString("Raspberry Pi Pico") ;
+    setCursor(65, 10) ;
+    writeString("Keypad demo") ;
+    setCursor(65, 20) ;
+    writeString("Hunter Adams") ;
+    setCursor(65, 30) ;
+    writeString("vha3@cornell.edu") ;
+    setCursor(250, 0) ;
+    setTextSize(2) ;
+    writeString("Key Pressed:") ;
+
+    // Map LED to GPIO port, make it low
+    gpio_init(LED) ;
+    gpio_set_dir(LED, GPIO_OUT) ;
+    gpio_put(LED, 0) ;
+
+    /////////////BEEP////////////
+
+    // Initialize SPI channel (channel, baud rate set to 20MHz)
+    spi_init(SPI_PORT, 20000000) ;
+    // Format (channel, data bits per transfer, polarity, phase, order)
+    spi_set_format(SPI_PORT, 16, 0, 0, 0);
+
+    // Map SPI signals to GPIO ports
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS, GPIO_FUNC_SPI) ;
+
+    // Map LDAC pin to GPIO port, hold it low (could alternatively tie to GND)
+    gpio_init(LDAC) ;
+    gpio_set_dir(LDAC, GPIO_OUT) ;
+    gpio_put(LDAC, 0) ;
+
+    // set up increments for calculating bow envelope
+    attack_inc = divfix(max_amplitude, int2fix15(ATTACK_TIME)) ;
+    decay_inc =  divfix(max_amplitude, int2fix15(DECAY_TIME)) ;
+
+    // Build the sine lookup table
+    // scaled to produce values between 0 and 4096 (for 12-bit DAC)
+    int ii;
+    for (ii = 0; ii < sine_table_size; ii++){
+         sin_table[ii] = float2fix15(2047*sin((float)ii*6.283/(float)sine_table_size));
+    }
+
+
+
+    ////////////////// KEYPAD INITS ///////////////////////
+    // Initialize the keypad GPIO's
+    gpio_init_mask((0x7F << BASE_KEYPAD_PIN)) ;
+    // Set row-pins to output
+    gpio_set_dir_out_masked((0xF << BASE_KEYPAD_PIN)) ;
+    // Set all output pins to low
+    gpio_put_masked((0xF << BASE_KEYPAD_PIN), (0x0 << BASE_KEYPAD_PIN)) ;
+    // Turn on pulldown resistors for column pins (on by default)
+    gpio_pull_down((BASE_KEYPAD_PIN + 4)) ;
+    gpio_pull_down((BASE_KEYPAD_PIN + 5)) ;
+    gpio_pull_down((BASE_KEYPAD_PIN + 6)) ;
+
+    // Add core 0 threads
+    pt_add_thread(protothread_core_0) ;
+
+    // Start scheduling core 0 threads
+    pt_schedule_start ;
+
+}
